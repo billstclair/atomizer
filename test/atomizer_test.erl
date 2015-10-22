b1d47f1f6ee6684013164ee23f22453dd8011cb5
Bill St. Clair <billstclair@gmail.com>
2015-10-22 16:27:13 -0400

    Switch from danw/erlsom to willemdj/erlsom. Make eunit test use fixed atom feed from this repo.

--- test/atomizer_test.erl
+++ test/atomizer_test.erl
@@ -3,11 +3,11 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("../src/atomizer.hrl").
 
--define(AtomFeedTest, "http://droid-toolbox.com/feed.atom").
+-define(AtomFeedTest, "https://raw.githubusercontent.com/billstclair/atomizer/master/test/priv/feed.atom")
 
 atomizer_test() ->
     Res = atomizer:parse_url(?AtomFeedTest),
     io:format("Res: ~p", [Res]),
     
-    ?assertEqual(Res#feed.title, "Recent Libraries"),
+    ?assertEqual(Res#feed.title, "Libraries for Android Devs"),
     ?assert(is_list(Res#feed.entries)).
